fda08f5768c1726f6f9ef43b3e741159484cee6d
mathias <mathias.paulenko@gmail.com>
2026-06-03 09:24:29 +0800

    fix: move CookieBanner inside BrowserRouter to fix router context crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { AppRouter } from './routers/AppRouter'
-import { CookieBanner } from './components/ui/CookieBanner'
 import { useKeyboardShortcuts } from './hooks/useKeyboardShortcuts'
 
 export const App = () => {
   useKeyboardShortcuts()
 
-  return (
-    <>
-      <AppRouter />
-      <CookieBanner />
-    </>
-  )
+  return <AppRouter />
 }
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HomePage } from '../components/pages/HomePage'
 import { PrivacyPage } from '../components/pages/PrivacyPage'
 import { NotFound } from '../components/pages/NotFound'
+import { CookieBanner } from '../components/ui/CookieBanner'
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <CookieBanner />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy" element={<PrivacyPage />} />
